eecc350483d0146e740056f4ad8741c1e56864aa
David Ventura <davidventura27@gmail.com>
2024-03-12 17:36:41 +0100

    fix wirechunk in dissector

--- dissector.lua
+++ dissector.lua
@@ -6,20 +6,25 @@ ilnk_proto.fields.refers_to = ProtoField.uint16("SnapCast.refers_to", "Refers to
 ilnk_proto.fields.sent_ts 	= ProtoField.uint64("SnapCast.sent_ts", "Sent timestamp")
 ilnk_proto.fields.recv_ts	= ProtoField.uint64("SnapCast.recv_ts", "Received timestamp")
 ilnk_proto.fields.size 		= ProtoField.uint32("SnapCast.size", "Size")
-ilnk_proto.fields.payload 	= ProtoField.bytes("SnapCast.payload", "Payload", base.DASH)
+ilnk_proto.fields.wirechunk_size 		= ProtoField.uint32("SnapCast.wirechunk_size", "Wire chunk size")
 
 function ilnk_proto.dissector(buffer, pinfo, tree)
     local packet_length = buffer:len()
 	local subtree = tree:add(ilnk_proto, buffer(), "SnapCast")
 	local size = buffer(22, 4)
-	subtree:add_le(ilnk_proto.fields.type, buffer(0, 2))
+	local type = buffer(0, 2)
+	subtree:add_le(ilnk_proto.fields.type, type)
 	subtree:add_le(ilnk_proto.fields.id, buffer(2, 2))
 	subtree:add_le(ilnk_proto.fields.refers_to, buffer(4, 2))
 	subtree:add_le(ilnk_proto.fields.sent_ts, buffer(6, 8))
 	subtree:add_le(ilnk_proto.fields.recv_ts, buffer(14, 8))
 	subtree:add_le(ilnk_proto.fields.size, size)
-	subtree:add(ilnk_proto.fields.payload, buffer(26, size:le_uint()))
 
 	local payload = ByteArray.tvb(buffer(26, size:le_uint()):bytes(), "Payload")
+
+	if type:le_uint() == 2 then
+		subtree:add_le(ilnk_proto.fields.wirechunk_size, buffer(34, 4))
+		local wirechunk = ByteArray.tvb(buffer(38, size:le_uint()-12):bytes(), "WireChunk")
+	end
 end
 tdp_table = DissectorTable.get("tcp.port"):add(1704, ilnk_proto)
