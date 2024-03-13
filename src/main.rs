60e48b277808d3380e3ed81a4fc0678da764319f
David Ventura <davidventura27@gmail.com>
2024-03-13 18:00:16 +0100

    start playing with the Time message

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,41 @@ use playback::{File, Player, Players};
 
 use std::collections::VecDeque;
 
-use proto::{Base, Server, ServerMessage};
+use proto::{Base, Server, ServerMessage, Time, TimeVal};
 
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::time;
 
 fn main() -> anyhow::Result<()> {
-    let srv = Server::new("11:22:33:44:55:66".into(), "framework".into());
-    let b = srv.hello();
     let mut s = TcpStream::connect("192.168.2.131:1704")?;
-    s.write_all(&b)?;
+    s.set_nodelay(true)?;
+
+    let srv = Server::new("11:22:33:44:55:66".into(), "framework".into());
+    {
+        let b = srv.hello();
+        s.write_all(&b)?;
+    }
+
+    let mut send_side = s.try_clone()?;
+
+    let start = time::Instant::now();
+
+    std::thread::spawn(move || {
+        let mut i: u16 = 0;
+        loop {
+            std::thread::sleep(time::Duration::from_millis(100));
+            let now = start.elapsed();
+            let tv = TimeVal {
+                sec: now.as_secs() as i32, // allows for 68 years of uptime
+                usec: now.subsec_micros() as i32,
+            };
+            println!("my delta {:?}", now);
+            let t = Time::as_buf(i, tv, tv, tv);
+            send_side.write(&t).unwrap();
+            i = i.wrapping_add(1);
+        }
+    });
 
     let mut dec: Option<Decoder> = None;
 
@@ -46,6 +71,8 @@ fn main() -> anyhow::Result<()> {
         match decoded_m {
             ServerMessage::CodecHeader(ch) => _ = dec.insert(Decoder::new(ch)?),
             ServerMessage::WireChunk(wc) => {
+                println!("wc ts {:?}", wc.timestamp);
+                // Guard against chunks coming before the decoder is initialized
                 if let Some(ref mut dec) = dec {
                     let s = dec.decode_sample(wc.payload, &mut samples_out)?;
 
